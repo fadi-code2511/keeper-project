a6e13d18ff11fb70fd09b36511e7625362791759
Fadi nasr <nasr.fadi2511@gmail.com>
2025-12-28 11:04:31 +0200

    making the notes dynamic

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
+
+
+ 
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((NoteItem)=>{
+        return  <div>
+                  <Note key={NoteItem.key}
+                  title={NoteItem.title} 
+                  content={NoteItem.content} />
+                </div>
+    })}
       <Footer />
     </div>
   );
