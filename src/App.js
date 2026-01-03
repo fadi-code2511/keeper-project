3e50e91f3e6136160271801adb96db3ed8166274
Fadi nasr <nasr.fadi2511@gmail.com>
2026-01-03 15:23:28 +0200

    feat/add note text to note card

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import Footer from "./components/Footer";
 import Note from "./components/Note";
 import notes from "./notes";
 import CreateArea from './components/CreateArea';
+import { useState } from "react";
+
 
 
  
 
 function App() {
+  const [items, setItem] = useState([]);
+  function addItem(noteText) {
+    setItem((prevValue) => {
+      console.log(items);
+      // event.preventDefault();
+      return [...prevValue, noteText];
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={0}
-      title={"NoteItem.title"} 
-      content={"NoteItem.content"} />
+      <CreateArea onAdd={addItem} />
+      {items.map((itemText, index) => {
+        return <Note key={index} title="Note title" content={itemText} />;
+      })}
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+function CreateArea(props) {
+  const [noteText, setNote] = useState("");
+  function handleClick(e) {
+    const noteValue = e.target.value;
+    console.log(noteValue);
+    setNote(noteValue);
+  }
 
-function CreateArea() {
   return (
     <div>
       <form>
         <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <textarea
+          onChange={handleClick}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={noteText}
+        />
+        <button
+          onClick={() => {
+            event.preventDefault();
+            props.onAdd(noteText);
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
